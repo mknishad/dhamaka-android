0d698c6e6a58d2f907e7772940cebefc6b576706
Monir Hossain <mknishad1@ymail.com>
2019-11-12 13:20:17 +0600

    init shopping cart toolbar

--- app/src/main/java/com/invariant/dhamaka/activity/ShoppingCartActivity.kt
+++ app/src/main/java/com/invariant/dhamaka/activity/ShoppingCartActivity.kt
@@ -1,12 +1,74 @@
 package com.invariant.dhamaka.activity
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.invariant.dhamaka.R
+import com.invariant.dhamaka.databinding.ActivityShoppingCartBinding
+import com.invariant.dhamaka.viewmodel.ShoppingCartViewModel
+import org.jetbrains.anko.toast
 
 class ShoppingCartActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityShoppingCartBinding
+    private lateinit var viewModel: ShoppingCartViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping_cart)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_shopping_cart)
+        viewModel = ViewModelProviders.of(this).get(ShoppingCartViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        init()
+    }
+
+    private fun init() {
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.toolbar.setTitleTextColor(resources.getColor(android.R.color.white, null))
+        } else {
+            binding.toolbar.setTitleTextColor(resources.getColor(android.R.color.white))
+        }
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.shoppint_cart)
+        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            binding.toolbar.navigationIcon?.colorFilter =
+                BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+        } else {
+            binding.toolbar.navigationIcon?.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_cart -> {
+                toast("Cart")
+                true
+            }
+            R.id.action_search -> {
+                toast("Search")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
