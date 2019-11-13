021c11281f0bbcb0373bf3618cc2d12d37471fc5
Saif <khaled.saif06@gmail.com>
2019-11-13 11:40:17 +0600

    Drawer layout added

--- app/src/main/java/com/invariant/dhamaka/activity/MainActivity.kt
+++ app/src/main/java/com/invariant/dhamaka/activity/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.invariant.dhamaka.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.navigation.NavigationView
+import com.invariant.dhamaka.Adapter.CustomExpandableListAdapter
 import com.invariant.dhamaka.R
 import com.invariant.dhamaka.databinding.ActivityMainBinding
+import com.invariant.dhamaka.databinding.ActivityMainTestBinding
 import com.invariant.dhamaka.viewmodel.MainViewModel
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.AnkoLogger
@@ -19,6 +30,55 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var carouselImageUrls: ArrayList<String>
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var categoryListView: ExpandableListView
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
+
+    private val data: HashMap<String, List<String>>
+        get() {
+            val listData = HashMap<String, List<String>>()
+
+            val redmiMobiles = ArrayList<String>()
+            redmiMobiles.add("Redmi Y2")
+            redmiMobiles.add("Redmi S2")
+            redmiMobiles.add("Redmi Note 5 Pro")
+            redmiMobiles.add("Redmi Note 5")
+            redmiMobiles.add("Redmi 5 Plus")
+            redmiMobiles.add("Redmi Y1")
+            redmiMobiles.add("Redmi 3S Plus")
+
+            val micromaxMobiles = ArrayList<String>()
+            micromaxMobiles.add("Micromax Bharat Go")
+            micromaxMobiles.add("Micromax Bharat 5 Pro")
+            micromaxMobiles.add("Micromax Bharat 5")
+            micromaxMobiles.add("Micromax Canvas 1")
+            micromaxMobiles.add("Micromax Dual 5")
+
+            val appleMobiles = ArrayList<String>()
+            appleMobiles.add("iPhone 8")
+            appleMobiles.add("iPhone 8 Plus")
+            appleMobiles.add("iPhone X")
+            appleMobiles.add("iPhone 7 Plus")
+            appleMobiles.add("iPhone 7")
+            appleMobiles.add("iPhone 6 Plus")
+
+            val samsungMobiles = ArrayList<String>()
+            samsungMobiles.add("Samsung Galaxy S9+")
+            samsungMobiles.add("Samsung Galaxy Note 7")
+            samsungMobiles.add("Samsung Galaxy Note 5 Dual")
+            samsungMobiles.add("Samsung Galaxy S8")
+            samsungMobiles.add("Samsung Galaxy A8")
+            samsungMobiles.add("Samsung Galaxy Note 4")
+
+            listData["Redmi"] = redmiMobiles
+            listData["Micromax"] = micromaxMobiles
+            listData["Apple"] = appleMobiles
+            listData["Samsung"] = samsungMobiles
+
+            return listData
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +92,72 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
     private fun init() {
         carouselImageUrls = ArrayList()
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        categoryListView = findViewById(R.id.category_list)
         setupToolbar()
         setupCarouselView()
+        setUpNavigation()
+        setCategoryListData()
+    }
+
+    private fun setUpNavigation() {
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+            toast("Test")
+            // Toast.makeText(this,"Test", Toast.LENGTH_LONG).show()
+            // Handle navigation view item clicks here.
+            when (menuItem.itemId) {
+
+                /*R.id.nav_profile -> {
+                    Toast.makeText(this, "Profile", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_wallet -> {
+                    Toast.makeText(this, "Wallet", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_offer -> {
+                    Toast.makeText(this, "Offer", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_setting -> {
+                    Toast.makeText(this, "Setting", Toast.LENGTH_LONG).show()
+                }*/
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            mDrawerLayout,
+            binding.toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ) {
+            override fun onDrawerClosed(view: View) {
+                super.onDrawerClosed(view)
+                //toast("Drawer closed")
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                //toast("Drawer opened")
+            }
+        }
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        mDrawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
     }
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
@@ -60,6 +180,23 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         }
     }
 
+    private fun setCategoryListData() {
+
+        val listData = data
+        titleList = ArrayList(listData.keys)
+        adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+        categoryListView!!.setAdapter(adapter)
+
+        categoryListView!!.setOnGroupExpandListener { groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Expanded.", Toast.LENGTH_SHORT).show() }
+
+        categoryListView!!.setOnGroupCollapseListener { groupPosition -> Toast.makeText(applicationContext, (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.", Toast.LENGTH_SHORT).show() }
+
+        categoryListView!!.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+            Toast.makeText(applicationContext, "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(titleList as ArrayList<String>)[groupPosition]]!!.get(childPosition), Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
@@ -67,6 +204,10 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
             R.id.action_cart -> {
                 toast("Cart")
                 true
